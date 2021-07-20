b19814bca784d798eb524c0155652dc23e3194cd
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-20 22:48:33 +0900

    単体テストの型作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe 'ユーザー新規登録' do
+    content '新規登録できる時' do
+      it 'すべての項目が想定通り入力されている場合登録できる' do
+
+      end
+      it 'パスワードが6文字以上だと登録できる' do
+
+      end
+    end
+    content '新規登録できない時' do
+      it 'ニックネームがないと登録できない' do
+
+      end
+      it 'メールアドレスがないと登録できない' do
+
+      end
+      it '重複したメールアドレスが存在する場合は登録できない' do
+
+      end
+      it 'メールアドレスは、@を含まないと登録できない' do
+
+      end
+      it 'パスワードがないと登録できない' do
+
+      end
+      
+      it 'パスワードが5文字以下だと登録できない' do
+
+      end
+      it 'パスワードは、半角英数字混合でないと入力できない' do
+
+      end
+      it 'パスワードとパスワード（確認）は、値の一致しないと登録できない' do
+
+      end
+    end
+  end
 end
