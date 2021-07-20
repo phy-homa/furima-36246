70c1dcae2f8e376a2d7b94b6300902c94fae1e60
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-20 22:26:42 +0900

    バリデーション修正

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Facker::initails(number:2)}
+    email                 {Facker::Internet.free_email}
+    password              {Facker::Internet.password}
+    password_confirmation {password}
+    first_name            {山田}
+    last_name             {太郎}
+    first_name_kana       {ヤマダ}
+    last_name_kana        {タロウ}
+    birth_date            {}
+  end
+end
