e899bf8b52c7ddf6204de853e37c52fca399c6ca
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-20 22:32:47 +0900

    factory修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Facker::initails(number:2)}
     email                 {Facker::Internet.free_email}
-    password              {Facker::Internet.password}
+    password              {Facker::Internet.password（min_length: 6)}
     password_confirmation {password}
     first_name            {山田}
     last_name             {太郎}
