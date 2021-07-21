f210c41225359c4628c1b958076b0f4e0038fe2a
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-21 17:27:11 +0900

    カテゴリーカラムのプルダウンメニュー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
     unless user_signed_in?
       redirect_to root_path
     end
+  end
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_bearer_id, :prefecture_id, :delivery_days_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
 
-belongs_to :item
+  with_options presence:true do |u|
+    u.validates :name
+    u.validates :description
+    u.validates :price
+  end
+  with_options numericality: {other_than:1, message:"can't be blank"} do |u|
+    u.validates :category_id
+    u.validates :status_id
+    u.validates :shipping_bearer_id
+    u.validates :prefecture_id
+    u.validates :delivery_days_id
+  end
+
+  belongs_to :item
+  belongs_to :category
+
 end
