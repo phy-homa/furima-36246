d5db01cf232806dbdfe72e1c6f7cd95defd55eac
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-21 14:41:33 +0900

    バリデーションの記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence:true do |u|
+    u.validates :nickname
+    u.validates :birth_date
+    u.validates :password_confirmation
+  end
+  with_options presence:true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ } do |u|
+    u.validates :first_name
+    u.validates :last_name
+  end
+  with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/ } do |u|
+    u.validates :first_name_kana
+    u.validates :last_name_kana
+  end
   validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, presence: true, format:{ with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true,  format:{ with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date, presence: true
-  validates :password_confirmation, presence: true
 
   has_many :items
 end
