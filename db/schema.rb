f027a1357a5c7dac6abfeb9f977b20c01a8a1742
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-21 17:04:16 +0900

    categoryモデル作成・カラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 ActiveRecord::Schema.define(version: 2021_07_20_053657) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_bearer_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_days_id", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
