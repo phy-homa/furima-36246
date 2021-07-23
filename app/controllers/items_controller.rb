f2437bbdd5e7f673e62429f99089ac6bb3c62ea1
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-23 17:25:21 +0900

    商品編集機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -25,6 +25,20 @@ class ItemsController < ApplicationController
     if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
+    end
+  end
+
+  def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
