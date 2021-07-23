7fa850b9a7f2c2a1deeb0ad87e49544843aeefb7
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-23 16:34:01 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
-    if current_user.id == @item.user_id
+    if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
     end
