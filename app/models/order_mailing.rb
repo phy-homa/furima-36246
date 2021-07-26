7ba81791a3624f4770c54ec2e23f1defee35ea21
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-26 10:55:32 +0900

    バリデーション設定

--- app/models/order_mailing.rb
+++ app/models/order_mailing.rb
@@ -2,11 +2,14 @@ class OrderMailing
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :tel, :order_id, :token
 
-  validates :postal_code, presence: true
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :tel, presence: true
+  with_options presence:true do |u|
+    u.validates :postal_code, format:{with: "/\A\d{3}[-]\d{4}\z/}"}
+    u.validates :prefecture_id
+    u.validates :city
+    u.validates :address
+    u.validates :tel,format:{with: "/\A0[0-9]{9,10}\z/"}
+    u.validates :token
+  end
 
 
   def save
